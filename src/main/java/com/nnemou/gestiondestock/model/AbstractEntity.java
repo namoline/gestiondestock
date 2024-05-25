41837a346443edd9086b877e27108b061c654d19
Nancy Nemouthe <mirman_759@sfr.fr>
2024-05-25 23:39:39 +0200

    first commit

---
+++ src/main/java/com/nnemou/gestiondestock/model/AbstractEntity.java
@@ -0,0 +1,9 @@
+package com.nnemou.gestiondestock.model;
+
+import java.io.Serializable;
+import javax.p
+public class AbstractEntity implements Serializable {
+
+	@Id
+	private Integer id;
+}
---
+++ src/test/java/com/nnemou/gestiondestock/GestiondestockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nnemou.gestiondestock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestiondestockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
